cb3ac9c4ce56dae196eea3405d04a21e6e61d4c7
Brian Welch <bdwelch1@gs66-rosa.ndc.nasa.gov>
2022-08-29 15:47:52 -0400

    added position optimization

--- KnotFitClass.py
+++ KnotFitClass.py
@@ -5,7 +5,7 @@ We'll see how this goes...
 
 
 TO-DO:
- - figure out multiprocessing within the class structure?
+ - add source plane position optimization
 
 '''
 
@@ -68,7 +68,7 @@ def chisquared(theta, **kwargs):
 
 
 def convolved(theta, **kwargs): 
-    # assume you have a list called "clumps" with instances of Clump class (I think this should work?)
+    # assume you have a list called "clumps" with instances of Clump class
     clumps = kwargs["clumps"]
     ind = 0
     for i in range(len(clumps)):
@@ -89,7 +89,7 @@ def convolved(theta, **kwargs):
         return convolve(galaxy_rebin, psf)
 
     elif resol == 'mixed': # be sure to run init_mixed_resolution before using this method!
-    # this gets suuuuper slow for now. I'll probably need to multiprocess to get this done in a reasonable time. Blarg!
+    # this gets suuuuper slow for now. 
         xss_hires = kwargs["xss_hires"]
         yss_hires = kwargs["yss_hires"]
         xslice_hires = kwargs["xslice_hires"]
@@ -181,22 +181,36 @@ class GalModel:
         return star
 
 
-    def set_sigma(self, rmsfile=None, exp_time=5000, sig=0):
+    def set_sigma(self, rmsfile=None, unit='eps',exp_time=1, photfnu=1e-8, gain=1.):
         if rmsfile:
             rmsf = fits.open(rmsfile)
             rms = rmsf[0].data
             rmscut = rms[self.ylo:self.yhi, self.xlo:self.xhi]
-            self.sigma = self.poisson(np.copy(self.arcim), t_expose=exp_time) + (rmscut*exp_time)
+            self.sigma = self.poisson(np.copy(self.arcim), 
+                                      t_expose=exp_time, 
+                                      unit=unit, 
+                                      gain=gain, 
+                                      photfnu=photfnu) + (rmscut*exp_time)
         else:
-            self.sigma = sig
+            self.sigma = 0.01 * self.poisson(np.copy(self.arcim), 
+                                      t_expose=exp_time, 
+                                      gain=gain, 
+                                      photfnu=photfnu) + 0.01
 
 
-    def poisson(self, flux_eps, t_expose, gain=1.):
-        ix = tuple([flux_eps<0])
-        flux_eps[ix] = 0. # remove zeros to avoid nans in sqrt
-        flux_photon = flux_eps / gain
-        Nphoton = flux_photon #* t_expose
-        result = np.sqrt(Nphoton) #/ t_expose
+    def poisson(self, flux, t_expose=1, gain=1., unit='eps', photfnu=1e-8):
+        if unit == 'eps':
+            ix = tuple([flux<0])
+            flux[ix] = 0. # remove zeros to avoid nans in sqrt
+            flux_photon = flux / gain
+            Nphoton = flux_photon #* t_expose
+            result = np.sqrt(Nphoton) #/ t_expose
+        elif unit == 'nJy':
+            ix = tuple([flux<0])
+            flux[ix] = 0. # remove zeros to avoid nans in sqrt
+            flux_electron = (flux * 1e-9) / photfnu # put in Jy, divide by Jy*sec/elec.
+            Nphoton = flux_electron * gain
+            result = np.sqrt(Nphoton)
         return result 
 
 
@@ -258,7 +272,7 @@ class GalModel:
             return convolve(galaxy_rebin, self.psf)
 
         elif self.resolution == 'mixed': # be sure to run init_mixed_resolution before using this method!
-        # this gets suuuuper slow for now. I'll probably need to multiprocess to get this done in a reasonable time. Blarg!
+        # this gets suuuuper slow for now. 
             galaxy = combined(self.xss, self.yss)
             galaxy_hires = combined(self.xss_hires, self.yss_hires)
             if self.super_res:
@@ -439,12 +453,12 @@ class Clump:
         self.amp = amp0
         self.r = r0
         if profile == "Gaussian":
-            self.theta = np.array([amp0, r0])
+            self.theta = np.array([amp0, r0, x0, y0])
         elif profile == "Gaussian-asym":
             self.rx = r0
             self.ry = r0
             self.rot_angle = rot_angle = 0.
-            self.theta = np.array([amp0, r0, r0, rot_angle])
+            self.theta = np.array([amp0, r0, r0, rot_angle, x0, y0])
         elif profile == "Delta":
             self.theta = np.array([x0, y0, amp0])
         elif profile == "Sersic":
@@ -452,7 +466,7 @@ class Clump:
             self.n = n0
             self.ellip = ellip = 0.
             self.rot_angle = rot_angle = 0.
-            self.theta = np.array([amp0, r0, n0, ellip, rot_angle])
+            self.theta = np.array([amp0, r0, n0, ellip, rot_angle, x0, y0])
         self.fixed = False
         self.profile = profile
 
@@ -472,21 +486,38 @@ class Clump:
 
     def prior(self):
         if self.profile == "Gaussian":
-            if (self.amin < self.amp < self.amax and self.rmin < self.r < self.rmax):
+            if (self.amin < self.amp < self.amax 
+                and self.rmin < self.r < self.rmax
+                and self.xmin < self.x < self.xmax 
+                and self.ymin < self.y < self.ymax):
                 return 0
             return -np.inf 
         elif self.profile == "Gaussian-asym":
-            if (self.amin < self.amp < self.amax and self.rmin < self.rx < self.rmax and self.rmin < self.ry < self.rmax 
-                and self.rot_min <= self.rot_angle <= self.rot_max):
+            rads = np.array([rx, ry])
+            ellip = np.sqrt(1 - (min(rads) / max(rads)))
+            if (self.amin < self.amp < self.amax 
+                and self.rmin < self.rx < self.rmax 
+                and self.rmin < self.ry < self.rmax 
+                and self.rot_min <= self.rot_angle <= self.rot_max
+                and self.emin <= ellip <= self.emax
+                and self.xmin < self.x < self.xmax 
+                and self.ymin < self.y < self.ymax):
                 return 0
             return -np.inf
         elif self.profile == "Delta":
-            if (self.amin < self.amp < self.amax and self.xmin < self.x < self.xmax and self.ymin < self.y < self.ymax):
+            if (self.amin < self.amp < self.amax 
+                and self.xmin < self.x < self.xmax 
+                and self.ymin < self.y < self.ymax):
                 return 0
             return -np.inf
         elif self.profile == "Sersic":
-            if (self.amin < self.amp < self.amax and self.rmin < self.r < self.rmax and self.nmin < self.n < self.nmax 
-                and self.emin < self.ellip < self.emax and self.rot_min <= self.rot_angle <= self.rot_max):
+            if (self.amin < self.amp < self.amax 
+                and self.rmin < self.r < self.rmax 
+                and self.nmin < self.n < self.nmax 
+                and self.emin < self.ellip < self.emax 
+                and self.rot_min <= self.rot_angle <= self.rot_max
+                and self.xmin < self.x < self.xmax 
+                and self.ymin < self.y < self.ymax):
                 return 0
             return -np.inf
 
@@ -519,11 +550,15 @@ class Clump:
             if self.profile == "Gaussian":
                 self.amp = theta[0]
                 self.r = theta[1]
+                self.x = theta[2]
+                self.y = theta[3]
             elif self.profile == "Gaussian-asym":
                 self.amp = theta[0]
                 self.rx = theta[1]
                 self.ry = theta[2]
                 self.rot_angle = theta[3]
+                self.x = theta[4]
+                self.y = theta[5]
             elif self.profile == "Delta":
                 self.x = theta[0]
                 self.y = theta[1]
@@ -534,6 +569,8 @@ class Clump:
                 self.n = theta[2]
                 self.ellip = theta[3]
                 self.rot_angle = theta[4]
+                self.x = theta[5]
+                self.y = theta[6]
 
     def get_num_params(self):
         return len(self.theta)
@@ -558,3 +595,5 @@ class Delta2D:
     
     def __call__(self, xx, yy):
         return self.evaluate(xx, yy, self.x0, self.y0, self.amp)
+
+
